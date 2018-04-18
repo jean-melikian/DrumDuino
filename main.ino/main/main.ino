16a2fa5dd6a5f24272e50c881c32df1d0b20da97
Keuwa <vincent.durpoix@hotmail.fr>
2018-04-18 12:05:10 +0200

    a

--- main.ino/main/main.ino
+++ main.ino/main/main.ino
@@ -1,5 +1,3 @@
-#include <Bounce2.h>
-
 const int buzz = 8;
 const int buzz2 = 9;
 const int recordPin = 7;
@@ -11,11 +9,14 @@ unsigned long previousMillis = 0;
 const long intervalLoop = 2;
 
 unsigned long previousMillisInterup = 0;
-const long intervalInterupt = 10000;
+const long intervalInterupt = 50;
 
-int recordCounts;
-int records[1000];
-unsigned long recordsTimer[1000];
+unsigned long previousMillisRecord = 0;
+const long intervalRecord = 100;
+
+int recordsCount;
+int records[100];
+unsigned long recordsTimer[100];
 
 enum state {
   RECORDING,
@@ -23,7 +24,7 @@ enum state {
   FREE_PLAY,
 };
 
-int currentState = RECORDING;
+int currentState = FREE_PLAY;
 
 
 // the setup routine runs once when you press reset:
@@ -31,31 +32,28 @@ void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   pinMode(recordPin, INPUT_PULLUP);
-  //attachInterrupt(digitalPinToInterrupt(recordPin), changeState, LOW);
+  attachInterrupt(digitalPinToInterrupt(recordPin), changeState, LOW);
 }
 
 
 void changeState() {
-  detachInterrupt(recordPin);
-  previousMillisInterup = 0;
-
-  if (currentState == RECORDING) {
+  if (previousMillisInterup >= intervalInterupt) {
+    if (currentState == RECORDING) {
     currentState = FREE_PLAY;
-  } else {
-    currentState = RECORDING;
+    } else {
+      currentState = RECORDING;
+    }
+    Serial.println("Switch");
   }
-  Serial.println("Switch");
-
+  previousMillisInterup = 0;
 }
 
 
 // the loop routine runs over and over again forever:
 void loop() {
 
-  previousMillisInterup += 1;
-  if (previousMillisInterup >= intervalInterupt) {
-    attachInterrupt(digitalPinToInterrupt(recordPin), changeState, LOW);
-  }
+  previousMillisInterup = previousMillisInterup  + 1;
+  
 
   switch (currentState) {
     case RECORDING:
@@ -66,15 +64,12 @@ void loop() {
       break;
   }
 
-  Serial.println(previousMillisInterup);
-
-
 }
 
 
 void record() {
   unsigned long currentMillis = millis();
-
+  previousMillisRecord ++;
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   int sensorValue2 = analogRead(A1);
@@ -83,21 +78,38 @@ void record() {
     previousMillis = currentMillis;
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-    if (sensorValue >= lastSensorValue + 20) {
+    
+    if (sensorValue >= lastSensorValue + 70) {
       noTone(buzz2);
       Serial.println("dafuck");
+      if(previousMillisRecord >= intervalRecord){
+        records[recordsCount] = buzz;
+        recordsTimer[recordsCount] = currentMillis;
+        recordsCount++;
+        previousMillisRecord = 0;
+      }
       tone(buzz, 523, 200);
     }
-
-    if (sensorValue2 >= lastSensorValue2 + 20) {
+    else if (sensorValue2 >= lastSensorValue2 + 70) {
       noTone(buzz);
       Serial.println("dafuck22");
+      if(previousMillisRecord >= intervalRecord){
+        records[recordsCount] = buzz2;
+        recordsTimer[recordsCount] = currentMillis;
+        recordsCount++; 
+        previousMillisRecord = 0;
+      }
       tone(buzz2, 900, 200);
     }
+    
   }
   lastSensorValue = sensorValue;
   lastSensorValue2 = sensorValue2;
-  //print out the value you read:
+  for (int i = 0;i<recordsCount;i++){
+    Serial.print(records[i]);  
+    Serial.print(" - ");  
+  }
+  Serial.println("__________");
 }
 
 void freePlay() {
@@ -112,13 +124,11 @@ void freePlay() {
 
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-    if (sensorValue >= lastSensorValue + 20) {
+    if (sensorValue >= lastSensorValue + 50) {
       noTone(buzz2);
       Serial.println("dafuck");
       tone(buzz, 523, 200);
-    }
-
-    if (sensorValue2 >= lastSensorValue2 + 20) {
+    }else if (sensorValue2 >= lastSensorValue2 + 50) {
       noTone(buzz);
       Serial.println("dafuck22");
       tone(buzz2, 900, 200);
@@ -128,5 +138,10 @@ void freePlay() {
   lastSensorValue = sensorValue;
   lastSensorValue2 = sensorValue2;
   //print out the value you read:
+  Serial.print(sensorValue);
+  Serial.print("-");
+  Serial.print(sensorValue2);
+  Serial.print("\n");
+  Serial.println(currentState);
 
 }
