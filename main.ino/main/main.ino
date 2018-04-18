a0fc7617e2058287f37016345503d18a2269f527
Keuwa <vincent.durpoix@hotmail.fr>
2018-04-18 10:53:36 +0200

    a

--- main.ino/main/main.ino
+++ main.ino/main/main.ino
@@ -1,30 +1,23 @@
-/*
-  AnalogReadSerial
-
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
-
-  This example code is in the public domain.
-
-  http://www.arduino.cc/en/Tutorial/AnalogReadSerial
-*/
+#include <Bounce2.h>
 
 const int buzz = 8;
 const int buzz2 = 9;
-const int recordPin = 10;
+const int recordPin = 7;
 
 int lastSensorValue = 0;
 int lastSensorValue2 = 0;
- 
-unsigned long previousMillis = 0;       
-const long interval = 2;
+
+unsigned long previousMillis = 0;
+const long intervalLoop = 2;
+
+unsigned long previousMillisInterup = 0;
+const long intervalInterupt = 10000;
 
 int recordCounts;
 int records[1000];
 unsigned long recordsTimer[1000];
- 
-enum state{
+
+enum state {
   RECORDING,
   RECORD_PLAYING,
   FREE_PLAY,
@@ -37,22 +30,34 @@ int currentState = RECORDING;
 void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
-  pinMode(recordPin,INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(recordPin), changeState, CHANGE);
+  pinMode(recordPin, INPUT_PULLUP);
+  //attachInterrupt(digitalPinToInterrupt(recordPin), changeState, LOW);
 }
 
+
 void changeState() {
-  if (currentState == RECORDING){
+  detachInterrupt(recordPin);
+  previousMillisInterup = 0;
+
+  if (currentState == RECORDING) {
     currentState = FREE_PLAY;
-  }else{
+  } else {
     currentState = RECORDING;
   }
   Serial.println("Switch");
+
 }
+
 
 // the loop routine runs over and over again forever:
 void loop() {
-  switch(currentState){
+
+  previousMillisInterup += 1;
+  if (previousMillisInterup >= intervalInterupt) {
+    attachInterrupt(digitalPinToInterrupt(recordPin), changeState, LOW);
+  }
+
+  switch (currentState) {
     case RECORDING:
       record();
       break;
@@ -61,30 +66,30 @@ void loop() {
       break;
   }
 
-    Serial.println(currentState);
+  Serial.println(previousMillisInterup);
 
 
 }
 
-void record() {
 
-  unsigned long currentMillis = millis(); 
-  
+void record() {
+  unsigned long currentMillis = millis();
+
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   int sensorValue2 = analogRead(A1);
-  if (currentMillis - previousMillis >= interval) {
-      
+  if (currentMillis - previousMillis >= intervalLoop) {
+
     previousMillis = currentMillis;
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-    if (sensorValue >= lastSensorValue + 20){
+    if (sensorValue >= lastSensorValue + 20) {
       noTone(buzz2);
       Serial.println("dafuck");
       tone(buzz, 523, 200);
     }
-    
-    if (sensorValue2 >= lastSensorValue2 + 20){
+
+    if (sensorValue2 >= lastSensorValue2 + 20) {
       noTone(buzz);
       Serial.println("dafuck22");
       tone(buzz2, 900, 200);
@@ -97,23 +102,23 @@ void record() {
 
 void freePlay() {
 
-  unsigned long currentMillis = millis(); 
-  
+  unsigned long currentMillis = millis();
+
   // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   int sensorValue2 = analogRead(A1);
-  if (currentMillis - previousMillis >= interval) {
+  if (currentMillis - previousMillis >= intervalLoop) {
     previousMillis = currentMillis;
 
     // save the last time you blinked the LED
     previousMillis = currentMillis;
-    if (sensorValue >= lastSensorValue + 20){
+    if (sensorValue >= lastSensorValue + 20) {
       noTone(buzz2);
       Serial.println("dafuck");
       tone(buzz, 523, 200);
     }
-    
-    if (sensorValue2 >= lastSensorValue2 + 20){
+
+    if (sensorValue2 >= lastSensorValue2 + 20) {
       noTone(buzz);
       Serial.println("dafuck22");
       tone(buzz2, 900, 200);
@@ -124,4 +129,4 @@ void freePlay() {
   lastSensorValue2 = sensorValue2;
   //print out the value you read:
 
-} 
+}
